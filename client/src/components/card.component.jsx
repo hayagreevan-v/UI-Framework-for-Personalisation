8f4a9e5296f81346118a27f3f274284256f38653
Jeciyazhini <jeciyazhinikumar@gmail.com>
2023-11-08 19:13:23 +0530

    card design

--- client/src/components/card.component.jsx
+++ client/src/components/card.component.jsx
@@ -1,6 +1,6 @@
 export default function Card(props) {
     return (
-        <div class={ props.class } style={{ boxShadow: "0px 30px 40px -20px blue",  backgroundColor: '#f9fbe7'}}>
+        <div class={ props.class } style={{ boxShadow: "0px 20px 40px -30px lightgrey",  background: 'linear-gradient(to top, #F6F6F6 0%, #ffffff 100%)'}}>
             <h2>{ props.title }</h2>
             <p>{ props.body }</p>
             <img src={ props.img } alt=""/>       
